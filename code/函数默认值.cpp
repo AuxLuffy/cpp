8d697ae7dd609706b64d7479255f5b9627bc58ed
sunzhangfei <sunzhangfei@zuoyebang.com>
2022-04-12 15:40:57 +0800

    函数默认值使用

--- code/函数默认值.cpp
+++ code/函数默认值.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+
+int func(int = 10, int = 20, int = 30);
 /**
  * @brief 注意：一个参数有默认值后，后面参数都要有默认值，否则程序无法识别哪一个是默认值
  *
@@ -8,7 +10,7 @@ using namespace std;
  * @param c
  * @return int
  */
-int func(int a, int b = 3, int c = 2)
+int func(int a, int b, int c)//声明中已经定义了默认值，实现中就不能再写了，否则会报错
 {
     return a + b + c;
 }
